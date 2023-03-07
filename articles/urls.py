a1517fc4a6dbf04dcb59985be525be3501c376ed
rrajesh0205 <r.rajesh@hotmail.ca>
2023-03-07 15:21:20 +0530

    Merged the two apps. 07.03.2023 at 03:21 PM

--- articles/urls.py
+++ articles/urls.py
@@ -4,10 +4,16 @@ from django.urls import path
 
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('', ArticleListView.as_view(), name='home'),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='detail'),
     path('create/', CreatePostView.as_view(), name='create'),
     path('create/<int:pk>/', ArticleUpdate.as_view(), name='article_edit'),
     path('delete/<int:pk>/', ArticleDelete.as_view(), name='delete'),  
     path('about/', ArticleAboutView.as_view(), name='about'),  
+    
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('test', views.test, name='test'),
+    path('logout', views.logout, name='logout'),
 ]
